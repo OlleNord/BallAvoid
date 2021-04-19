a48ff5ca0a65f7eaa4b06d18c659fa127f202ec6
OlleNord <72620544+OlleNord@users.noreply.github.com>
2021-04-19 17:28:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const ctx = canvas.getContext("2d");
 
 const MARGINTOP = window.innerHeight/10;
 const MARGINRIGHT = window.innerWidth/8;
-const MARGINBOTTOM = window.innerHeight/8;
-const MARGINLEFT = window.innerWidth/10;
+const MARGINBOTTOM = window.innerHeight/10;
+const MARGINLEFT = window.innerWidth/8;
 
 const WIDTH = window.innerWidth-((MARGINRIGHT+MARGINLEFT)+(2));
 const HEIGHT = window.innerHeight-((MARGINTOP+MARGINBOTTOM)+(2));
@@ -13,39 +13,94 @@ canvas.height = HEIGHT;
 
 document.body.style.margin = MARGINTOP.toString() + "px " + MARGINRIGHT.toString() + "px " + MARGINBOTTOM.toString() + "px " + MARGINLEFT.toString() + "px";
 
+window.addEventListener("resize", function() {
+    window.location.reload();
+})
+
+var mouse = {
+    x: 0,
+    y: 0,
+    click: false
+}
+var canvasPos = canvas.getBoundingClientRect();
+canvas.addEventListener("mousedown", function(e){
+    mouse.x = e.x - canvasPos.left;
+    mouse.y = e.y - canvasPos.top;
+    console.log(mouse.x, mouse.y);
+});
+
 class Player {
-    constructor(x, y, w, h, c) {
+    constructor(x, y, r, c) {
         this.x = x;
         this.y = y;
-        this.w = w;
-        this.h = h;
+        this.r = r;
+        this.c = c;
+    }
+
+    Update() {
+        const dx = this.x - mouse.x;
+        const dy = this.y - mouse.y;
+        if (mouse.x != this.x) {
+            this.x -= dx/20;
+        }
+        if (mouse.y != this.y) {
+            this.y -= dy/20;
+        }
+        
+    }
+
+    Draw() {
+        ctx.fillStyle = this.c;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+        ctx.fill();
+        ctx.closePath();
+    }
+    
+}
+
+class Food {
+    constructor(x, y, r, c) {
+        this.x = Math.random() * WIDTH;
+        this.y = y;
+        this.r = r;
         this.c = c;
     }
 
     Draw() {
         ctx.fillStyle = this.c;
-        ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+        ctx.fill();
+        ctx.closePath();
     }
+
+    Check() {
+        if (Math.abs(player.x - this.x) < this.r*2 && Math.abs(player.y - this.y) < this.r*2) {
+            return true;
+        }
+    }
+    
 }
 
-class Food {
-    constructor(x, y, w, h, c) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.c = c;
-    }
+var player = new Player(0, 0, 10, "#93C4F6");
+var Foods = [];
 
-    Draw() {
-        ctx.fillStyle = c;
-        ctx.fillRect(this.x, this.y, this.w, this.h);
-    }
+for (i = 0; i < 10; i++) {
+    Foods[i] = new Food(0, 10, 10, "red");
 }
-
-var player = new Player(0, 0, 10, 10, "Red");
-
 function Update() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     player.Draw();
+    player.Update();
+    for (i = 0; i < 10; i++) {
+        Foods[i].Draw();
+        if (Foods[i].Check()) {
+            console.log("Collision with " + i);
+            Foods.splice(i, 1);
+        }
+    }
+    requestAnimationFrame(Update);
 }
+
+Update();
