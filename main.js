3b4ab8842abdfa750a127c059c95cfd1f16c2298
OlleNord <72620544+OlleNord@users.noreply.github.com>
2021-05-01 16:56:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -14,22 +14,29 @@ canvas.height = HEIGHT;
 console.log(WIDTH);
 console.log(canvas.width);
 
-document.body.style.margin = MARGINTOP.toString() + "px " + MARGINRIGHT.toString() + "px " + MARGINBOTTOM.toString() + "px " + MARGINLEFT.toString() + "px";
+document.getElementById("c").style.margin = MARGINTOP.toString() + "px " + MARGINRIGHT.toString() + "px " + MARGINBOTTOM.toString() + "px " + MARGINLEFT.toString() + "px";
 
 window.addEventListener("resize", function() {
     window.location.reload();
 })
 
 var mouse = {
-    x: 0,
-    y: 0,
+    x: WIDTH/2,
+    y: HEIGHT/2,
     click: false
 }
+
+var colors = ["#FF0000", "#e50000", "#51C651", "#007300", "#0000FF", "#0000E5", "#FFE62F"];
+
 var canvasPos = canvas.getBoundingClientRect();
 canvas.addEventListener("mousedown", function(e){
     mouse.x = e.x - canvasPos.left;
     mouse.y = e.y - canvasPos.top;
-    console.log(mouse.x, mouse.y);
+});
+canvas.addEventListener("mousedown", function(e){
+    console.log("Mouse x: " + mouse.x, "Mouse y: " + mouse.y);
+    console.log("Event x: " + e.x, "Event y: " + e.y);
+    console.log("Player x: " + Math.round(player.x), "Player y: " + Math.round(player.y));
 });
 
 class Player {
@@ -41,13 +48,27 @@ class Player {
     }
 
     Update() {
+        
         const dx = this.x - mouse.x;
         const dy = this.y - mouse.y;
+
         if (mouse.x != this.x) {
             this.x -= dx/20;
+            if (this.x > WIDTH-this.r) {
+                this.x = WIDTH-this.r+0.001;
+            }
+            if (this.x < 0+this.r) {
+                this.x = 0+this.r+0.001;
+            }
         }
         if (mouse.y != this.y) {
             this.y -= dy/20;
+            if (this.y > HEIGHT-this.r) {
+                this.y = HEIGHT-this.r+0.001;
+            }
+            if (this.y < 0+this.r) {
+                this.y = 0+this.r-0.001;
+            }
         }
         
     }
@@ -64,9 +85,9 @@ class Player {
 
 class Food {
     constructor(x, y, r, c) {
-        this.x = Math.floor(Math.random() * WIDTH-r) + 1+r;
-        this.y = Math.floor(Math.random() * HEIGHT) + 1;
         this.r = r;
+        this.x = (Math.random() * (WIDTH-this.r*2)) + this.r*1;
+        this.y = (Math.random() * (HEIGHT-this.r*2)) + this.r*1;;
         this.c = c;
     }
 
@@ -80,21 +101,27 @@ class Food {
 
     Check() {
         if (Math.abs(player.x - this.x) < this.r*2 && Math.abs(player.y - this.y) < this.r*2) {
+            collected++;
             return true;
         }
     }
     
 }
 
-var player = new Player(0, 0, 10, "#93C4F6");
+var time = 3;
+var collected = 0;
+var player = new Player(WIDTH/2, HEIGHT/2, 10, colors[5]);
 var Foods = [];
 
-for (i = 0; i < 10; i++) {
-    Foods.push(new Food(0, 10, 10, "red"));
+for (i = 0; i < 25; i++) {
+    Foods.push(new Food(0, 10, 10, colors[2]));
 }
 console.log(WIDTH);
 function Update() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    ctx.font = WIDTH/20 + "px Verdana";
+    ctx.fillStyle = colors[6];
+    ctx.fillText(time, WIDTH/2-(WIDTH/20)/2, HEIGHT/2+(HEIGHT/20)/2);
     player.Draw();
     player.Update();
     for (i = 0; i < Foods.length; i++) {
@@ -104,7 +131,22 @@ function Update() {
             Foods.splice(i, 1);
         }
     }
-    requestAnimationFrame(Update);
+    if (!time == 0) {
+        requestAnimationFrame(Update);
+    }
+    else {
+        GameOver();
+    }
+}
+
+function Timer() {
+    time -= 1;
+}
+
+function GameOver() {
+    ctx.fillStyle = colors[6];
+    ctx.fillText("Game Over", WIDTH/2-(WIDTH/8), HEIGHT/2-(HEIGHT/10)/2);
 }
 
 Update();
+setInterval(Timer, 1000);
