62efcf5ef0ba738dc5645b20f89ea0b6162f04ee
OlleNord <72620544+OlleNord@users.noreply.github.com>
2021-05-15 18:32:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+licens = prompt("You need licens-id: ");
+if (licens != "1233") {
+    window.location = "http://google.com/";
+}
 const canvas = document.getElementById("c");
 const ctx = canvas.getContext("2d");
 
@@ -108,29 +112,78 @@ class Food {
     
 }
 
+class Enemy {
+    constructor(x, y, r, c) {
+        this.r = r;
+        this.x = (Math.random() * (WIDTH-this.r*2)) + this.r*1;
+        this.y = (Math.random() * (HEIGHT-this.r*2)) + this.r*1;
+        this.c = c;
+        this.vx = ((Math.random() * 10)-5)*2;
+        this.vy = ((Math.random() * 10)-5)*2;
+        console.log(this.x);
+    }
+    Draw() {
+        ctx.fillStyle = this.c;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+        ctx.fill();
+        ctx.closePath();
+    }
+    Update() {
+        
+        if (this.x > WIDTH-this.r) {
+            this.vx *= -1;
+        }
+        if (this.x < 0+this.r) {
+            this.vx *= -1;
+        }
+        if (this.y > HEIGHT-this.r) {
+            this.vy *= -1;
+        }
+        if (this.y < 0+this.r) {
+            this.vy *= -1;
+        }
+        this.x += this.vx;
+        this.y += this.vy;
+        console.log(this.x, this.y);
+    }
+    Check() {
+        if (Math.abs(player.x - this.x) < this.r*2 && Math.abs(player.y - this.y) < this.r*2) {
+            time = 0;
+            GameOver();
+        }
+    }
+}
+
 var time = 12;
-var highscore = localStorage.getItem("highscore");
+var highscore = localStorage.getItem("highscore_ballavoid");
 var collected = 0;
 var numFoods = 25;
+var numEnemies = 6;
 var radius = (WIDTH+HEIGHT)/300;
 var player = new Player(WIDTH/2, HEIGHT/2, radius, colors[5]);
 var Foods = [];
+var Enemies = [];
 
 for (i = 0; i < numFoods; i++) {
     Foods.push(new Food(0, 10, radius, colors[2]));
+}
+
+for (i = 0; i < numEnemies; i++) {
+    Enemies.push(new Enemy(100, 10, radius, colors[0]));
 }
 console.log(WIDTH);
 function Update() {
     if (collected > highscore) {
         highscore = collected;
-        localStorage.setItem("highscore", highscore);
+        localStorage.setItem("highscore_ballavoid", highscore);
     }
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     ctx.font = WIDTH/20 + "px Verdana";
     ctx.fillStyle = colors[6];
     ctx.fillText(time, WIDTH/2-(WIDTH/20)/2, HEIGHT/2+(HEIGHT/20)/2);
     ctx.fillText(collected+"/"+numFoods, 50, 100);
-    ctx.fillText("Highscore: " + localStorage.getItem("highscore"), 50, 250);
+    ctx.fillText("Highscore: " + localStorage.getItem("highscore_ballavoid"), 50, 250);
     player.Draw();
     player.Update();
     for (i = 0; i < Foods.length; i++) {
@@ -139,6 +192,11 @@ function Update() {
             console.log("Collision with " + i);
             Foods.splice(i, 1);
         }
+    }
+    for (i = 0; i < Enemies.length; i++) {
+        Enemies[i].Draw();
+        Enemies[i].Update();
+        Enemies[i].Check();
     }
     if (!time == 0) {
         requestAnimationFrame(Update);
@@ -158,7 +216,7 @@ function GameOver() {
     ctx.fillText("Game Over", WIDTH/2-(WIDTH/8), HEIGHT/2-(HEIGHT/10)/2);
     if (collected > highscore) {
         highscore = collected;
-        localStorage.setItem("highscore", highscore);
+        localStorage.setItem("highscore_ballavoid", highscore);
     }
 
 }
